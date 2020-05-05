377694290a2ae03ab7b9f2dd28ddb9afac73721e
Dominik Hettwer <dominikhettwer@yahoo.de>
2020-05-05 08:37:32 +0200

    Setup base project

--- src/main/java/group/MainApp.java
+++ src/main/java/group/MainApp.java
@@ -33,33 +33,4 @@ public class MainApp extends Application {
         // Show the stage
         stage.show();
     }
-
-//    /**
-//     * Creates the scene with the FXMLLoader from a fxml file, executes custom initializations for the controller and
-//     * displays the scene on the stage.
-//     *
-//     * @param stage The primary stage where the scenes will be shown.
-//     * @throws Exception Throws all occurring exceptions.
-//     */
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//        // Create FXML loader and load the fxml file
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/scenes/base_scene.fxml"));
-//
-//        // Create the root object from the loader
-//        Parent root = loader.load();
-//
-//        // Get the connected controller
-//        BaseSceneController controller = loader.getController();
-//
-//        // Call custom initialize function
-//        controller.initializeController("Different Text, same Exit!");
-//
-//        // Set stage title and size
-//        stage.setTitle("Hello JavaFX!");
-//        stage.setScene(new Scene(root, 300, 150));
-//
-//        // Show the stage
-//        stage.show();
-//    }
 }
