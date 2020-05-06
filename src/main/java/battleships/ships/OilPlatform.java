893d8c22ad3e12d806db34a5944e848fa016261a
Dominik Hettwer <dominikhettwer@yahoo.de>
2020-05-06 12:09:12 +0200

    Alter OilPlatform placement

--- src/main/java/battleships/ships/OilPlatform.java
+++ src/main/java/battleships/ships/OilPlatform.java
@@ -16,11 +16,13 @@ public class OilPlatform extends Ship {
     @Override
     public void setCoordinates(boolean isHorizontal, Field position) throws Exception {
         for (int l = 1; l < this.length; l++) {
-            if ((position.getX() + length) > 10 || (position.getY() + length) > 10) {
+            if ((position.getX() + length) > 9 || (position.getY() + length) > 9) {
                 throw new Exception(OUT_OF_BOUND_EXCEPTION_CAUSE);
             } else {
                 this.occupiedCoordinates.add(new Field(position.getX() + l, position.getY()));
-                this.occupiedCoordinates.forEach(oc -> new Field(oc.getX() + 1, oc.getY() + 1));
+                for (Field test : occupiedCoordinates) {
+                    occupiedCoordinates.add(new Field(test.getX() + 1, test.getY() + 1));
+                }
             }
         }
     }
